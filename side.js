e403c3e224e29acfb8b43c4a8fe8564866ff396f
huahua-water <57286062+huahua-water@users.noreply.github.com>
2020-03-11 20:57:58 +0800

    Delete side.js

--- side.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$('#open-side-nav, #close-side-nav').click(function(event) {
-		event.preventDefault();
-		$('#main, #side-nav').toggleClass('off-display');
-	});
-});
